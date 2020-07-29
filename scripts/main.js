d1361bca7eee64907a0327566d871e31f025abee
david dehe <david.dehe.pro@gmail.com>
2020-07-29 06:02:00 +0200

    update_hp

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// const canvas = document.querySelector("canvas");
+// const ctx = canvas.getContext("2d");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// draw_canvas=()=>{
+// ctx.fillStyle = "white";
+// ctx.beginPath();
+// ctx.moveTo(150, 100);
+// ctx.lineTo(200, 150);
+// ctx.lineTo(200, 200);
+// ctx.lineTo(150, 250);
+// ctx.lineTo(100, 250);
+// ctx.lineTo(50, 200);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(100, 100);
+// ctx.closePath();
+// ctx.fill();
+// }
+// draw_canvas()
+
+
+// window.addEventListener("resize",()=>{
+//     canvas.width = window.innerWidth 
+//     canvas.height = window.innerHeight;
+//     draw_canvas();
+//   })
+
+
