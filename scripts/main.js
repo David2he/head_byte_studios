7d1875d856387e69230d80b2ec2c2e8f82d37c4d
david dehe <david.dehe.pro@gmail.com>
2020-07-29 23:13:48 +0200

    start to think responsive
    handle aspect ratio with java script instead of css.
    I will probably use the width and the height of the screen to scale the shape of background

--- scripts/main.js
+++ scripts/main.js
@@ -27,4 +27,37 @@
 //     draw_canvas();
 //   })
 
+let $link_center = document.querySelector('.svg_center_container_link')
+// let test_var = "--scale_hexa_w";
+// // $link_center.addEventListener('click',()=>{
+// //     var style = getComputedStyle(document.body);
+// //     console.log(style.getPropertyValue('--scale_hexa_w'));
+// //     document.documentElement.style.setProperty('--scale_hexa_w', '5');
+// //     console.log(style.getPropertyValue('--scale_hexa_w'));
+// // })
+
+// $link_center.addEventListener('click',()=>{
+//     document.documentElement.style.setProperty(test_var, '8')
+//     console.log( document.documentElement.style.getPropertyValue(test_var));
+// })
+let i  = window.innerWidth *0.0002
+let i2  = window.innerHeight * 0.0004
+
+
+let $svg = document.querySelector('.testouille')
+
+$link_center.addEventListener('click',()=>{
+    $svg.style.transform =   `scale(${i}, ${i2})`;
+
+    }
+)
+
+window.addEventListener('resize', ()=>{
+    console.log(window.innerWidth)
+    console.log(window.innerHeight)
+    // i += 0.1
+    // i2 += 0.1
+    $svg.style.transform =   `scale(${i}, ${i2})`;
+})
+
 
