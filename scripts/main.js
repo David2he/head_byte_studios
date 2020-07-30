7725097103e77fdc1bdfddb3e5bc6ec8420c82a1
david dehe <david.dehe.pro@gmail.com>
2020-07-30 21:30:54 +0200

    add new transition and fix an issue with swup and js
    Swup allow the website to do not refresh the website, so it means the script wont too.  I find a way to refresh the good part of script for each page

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,58 @@
-const swup = new Swup();
-let $svg = document.querySelector('.svg_bg')
-let $all_path = $svg.querySelectorAll('.js_path_bg')
+const swup = new Swup({
+    plugins: [new SwupOverlayTheme({
+        color: '#df8d52',
+        duration: 500,
+        direction: 'to-bottom',
+    })]
+  });
+// let $svg = document.querySelector('.svg_bg')
+// let $all_path = $svg.querySelectorAll('.js_path_bg')
 
-test =()=>{
-    for(i = 0; i < $all_path.length; i++){
-        random = Math.floor(Math.random(1)*$all_path.length)
-        // console.log(random)
-        $all_path[random].classList.toggle('orange_test')
-        // console.log('coucou')
+// test =()=>{
+//     for(i = 0; i < $all_path.length; i++){
+//         random = Math.floor(Math.random(1)*($all_path.length/2))
+//         // console.log(random)
+//         $all_path[random].classList.toggle('orange_test')
+//         // console.log('coucou')
+//     }
+// }
+// setInterval(test, 2100)
+// // setInterval(test, 2800)
+
+
+
+
+function init_first_page() {
+    if(document.querySelector('.svg_bg') != null) {
+        let $svg = document.querySelector('.svg_bg')
+        let $all_path = $svg.querySelectorAll('.js_path_bg')
+
+        test =()=>{
+            for(i = 0; i < $all_path.length; i++){
+                random = Math.floor(Math.random(1)*($all_path.length/2))
+                // console.log(random)
+                $all_path[random].classList.toggle('orange_test')
+                // console.log('coucou')
+            }
+        }
+        setInterval(test, 2100)
+        // setInterval(test, 2800)
     }
 }
-setInterval(test, 1000)
 
+
+init_first_page();
+document.addEventListener('swup:contentReplaced', init_first_page);
+
+
+
+
+function init() {
+    if(document.querySelector(".link_test") != null) {
+        console.log('coucou')
+    }
+}
+
+
+init();
+document.addEventListener('swup:contentReplaced', init);
