d34d315a5694d0e3e1274aa03992e19a0be71b9b
david dehe <david.dehe.pro@gmail.com>
2020-07-30 05:03:57 +0200

    add library swup
    i just install and fix problem with swup (a library to handle transition) i do not finish yet the transition i want i just "install" the library and make it work with the project . I add logo and a second page

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,14 @@
-let $link_center = document.querySelector('.svg_center_container_link')
-// let test_var = "--scale_hexa_w";
-// // $link_center.addEventListener('click',()=>{
-// //     var style = getComputedStyle(document.body);
-// //     console.log(style.getPropertyValue('--scale_hexa_w'));
-// //     document.documentElement.style.setProperty('--scale_hexa_w', '5');
-// //     console.log(style.getPropertyValue('--scale_hexa_w'));
-// // })
+const swup = new Swup();
+let $svg = document.querySelector('.svg_bg')
+let $all_path = $svg.querySelectorAll('.js_path_bg')
 
-// $link_center.addEventListener('click',()=>{
-//     document.documentElement.style.setProperty(test_var, '8')
-//     console.log( document.documentElement.style.getPropertyValue(test_var));
-// })
+test =()=>{
+    for(i = 0; i < $all_path.length; i++){
+        random = Math.floor(Math.random(1)*$all_path.length)
+        // console.log(random)
+        $all_path[random].classList.toggle('orange_test')
+        // console.log('coucou')
+    }
+}
+setInterval(test, 1000)
 
-
-// let i  = window.innerWidth *0.000274 - window.innerHeight * 0.000309
-// let i2  = window.innerHeight * 0.000849 - window.innerWidth * 0.000274
-
-// let i  = window.innerWidth *0.000142
-// let i2  = window.innerHeight * 0.000309
-
-// // console.log(i)
-// // console.log(i2)
-
-
-// let $svg = document.querySelector('.testouille')
-// // $svg.style.transform =   `scale(${i}, ${i2})`;
-
-// // $link_center.addEventListener('click',()=>{
-// //     $svg.style.transform =   `scale(${i}, ${i2})`;
-
-// //     }
-// // )
-
-
-// // window.addEventListener('resize', ()=>{
-// //     console.log(window.innerWidth)
-// //     console.log(window.innerHeight)
-// //     if(window.innerHeight > stock_height)
-// //     $svg.style.transform =   `scale(${i}, ${i2})`;
-// // }
-// // )
-// let x_scale = 1.2; 
-
-// let stock_width =window.innerWidth
-// let stock_height = window.innerHeight
-// console.log(stock_height)
-// window.addEventListener('resize', ()=>{
-//     if(window.innerHeight < stock_height){
-//         x_scale -=0.01
-//         console.log('pute')
-//         $svg.style.transform = `scaleX(${x_scale})`
-//     }
-//     stock_width =window.innerWidth
-//     stock_height = window.innerHeight 
-// })
